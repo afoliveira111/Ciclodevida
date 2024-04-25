794f82780efb66bf5dd9a57b791e43280cd81244
Felipe <felipetermometria@hotmail.com>
2024-04-24 22:32:44 -0300

    Atulizando com o onSave para não perder dados quando tela é rotacionada.

--- app/src/main/java/com/example/ciclodevida/MainActivity.kt
+++ app/src/main/java/com/example/ciclodevida/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.ciclodevida
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,11 +13,31 @@ private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
+    var counter = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG,"OnCreate chamado")
+
+
+        val tvCount = findViewById<TextView>(R.id.tv_count)
+        val btnCount = findViewById<Button>(R.id.btn_count)
+        if (savedInstanceState != null){
+            counter = savedInstanceState.getInt("myCount")
+            tvCount.text = counter.toString()
+        }
+
+        btnCount.setOnClickListener{
+            counter = counter + 1
+            tvCount.text = counter.toString()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("myCount", counter)
+        super.onSaveInstanceState(outState)
     }
 
     override fun onStart() {
